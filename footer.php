4c2ebe21ed76deb2a91e8badc508d66fa8ad7862
Tim Wu <timwuchia@gmail.com>
2019-04-11 18:27:16 -0700

    updates

--- footer.php
+++ footer.php
@@ -11,27 +11,14 @@
 
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'hip-creation' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'hip-creation' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-			<?php echo 	'the footer again'; ?>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'hip-creation' ), 'hip-creation', '<a href="http://nessie-tim">Tim and Nessie</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer">
+
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
-<?php wp_footer(); ?>
+<?php //wp_footer(); ?>
 
 </body>
 </html>
